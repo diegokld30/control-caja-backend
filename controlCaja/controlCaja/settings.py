5bfcc041b157614411a68eacf806b6f9cfaf47d4
diegokld30 <dfercalderon@poligran.edu.co>
2025-03-27 18:38:35 -0500

    Se cambio el tiempo de vencimiento del token de acceso a 8 horas

--- controlCaja/controlCaja/settings.py
+++ controlCaja/controlCaja/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=480),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
